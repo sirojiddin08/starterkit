6f994631d4e4819849393f896f767c5ad0ea3062
Sirojiddin <sirojiddin.devs@gmail.com>
2025-04-03 19:23:07 +0500

    fix auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,19 +1,11 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import createError from "http-errors";
 import { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
 import env from "../config/env";
 
-const prisma = new PrismaClient();
 const { ACCESS_TOKEN_SECRET } = env;
 
-// Extend Express Request type to include user properties
-interface CustomRequest extends Request {
-    user_id?: number;
-    role_id?: number;
-}
-
-const isAuthenticated = async (req: CustomRequest, res: any, next: NextFunction) => {
+const isAuthenticated = async (req: any, res: any, next: NextFunction) => {
     try {
         const authToken = req.get("Authorization");
         const accessToken = authToken?.split("Bearer ")[1];
@@ -29,27 +21,7 @@ const isAuthenticated = async (req: CustomRequest, res: any, next: NextFunction)
             return next(createError.Unauthorized("Invalid or expired token."));
         }
 
-        const { id, unique_id, role_id } = decodedToken;
-
-        // Query the database using Prisma ORM
-        const userSession = await prisma.user_sessions.findFirst({
-            where: {
-                user_id: id,
-                unique_id: unique_id,
-            },
-            include: {
-                users: {
-                    select: {
-                        id: true,
-                        role_id: true,
-                    },
-                },
-            },
-        });
-
-        if (!userSession || !userSession.users) {
-            return next(createError.Unauthorized("Invalid or expired token."));
-        }
+        const { id, role_id } = decodedToken;
 
         // Attach user info to the request object
         req.user_id = id;
